3b0ac5dd745482ced76b98a1345e94db898f2fcd
Heloisa Monteiro Cadernal <heloisa.cadernal@eaportal.org>
2025-04-06 18:38:07 -0300

    criaçaõ da função para remover a classe button selceted e inicio da lógica do custom tip

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0 
 let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue(){
     bill = document.querySelector("#bill").valueAsNunber 
@@ -9,4 +10,11 @@ function receiveBillValue(){
 
 function receiveNumberOfPeople(){
     numberOfPeople = document.querySelector("#people").valueAsNunber
+}
+
+function receiveTipPercentageValue(value){
+    tipPercentage = value / 100
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
 }
